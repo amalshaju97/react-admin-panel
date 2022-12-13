c1e6fc5e7d30ecd6efd6273094a8444651fdb6e2
Amal Shaju <amalpuliyani@gmail.com>
2022-12-13 12:38:05 +0530

    removed extras

--- src/_common/layout/components/aside/AsideMenuMain.tsx
+++ src/_common/layout/components/aside/AsideMenuMain.tsx
@@ -1,7 +1,6 @@
 /* eslint-disable react/jsx-no-target-blank */
 import React from 'react'
 import {useIntl} from 'react-intl'
-import {KTSVG} from '../../../helpers'
 import {AsideMenuItemWithSub} from './AsideMenuItemWithSub'
 import {AsideMenuItem} from './AsideMenuItem'
 
@@ -110,18 +109,6 @@ export function AsideMenuMain() {
         <div className='menu-content'>
           <div className='separator mx-1 my-4'></div>
         </div>
-      </div>
-      <div className='menu-item'>
-        <a
-          target='_blank'
-          className='menu-link'
-          href={process.env.REACT_APP_PREVIEW_DOCS_URL + '/docs/changelog'}
-        >
-          <span className='menu-icon'>
-            <KTSVG path='/media/icons/duotune/general/gen005.svg' className='svg-icon-2' />
-          </span>
-          <span className='menu-title'>Changelog {process.env.REACT_APP_VERSION}</span>
-        </a>
       </div>
     </>
   )
